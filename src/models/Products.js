5c56dd814d35daa17a6c3dbe499dc2c6679ce69c
M. Raddaoui <140417247+mr-hookie-repo@users.noreply.github.com>
2024-08-25 10:22:04 -0400

    add comments for clarity

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,7 +1,20 @@
 class Products {
+    /**
+     * @class Products
+     * @classdesc Model for a product
+     */
     constructor(code, name, price) {
+      /**
+       * @property {string} code - Code of the product
+       */
       this.code = code;
+      /**
+       * @property {string} name - Name of the product
+       */
       this.name = name;
+      /**
+       * @property {number} price - Price of the product
+       */
       this.price = price;
     }
   }
