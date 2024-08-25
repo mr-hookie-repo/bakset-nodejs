d8e56b453f7c994e2180ad6af13eacc7bff636a9
M. Raddaoui <140417247+mr-hookie-repo@users.noreply.github.com>
2024-08-25 10:20:52 -0400

    added comments for more clarity

--- src/models/Offers.js
+++ src/models/Offers.js
@@ -1,7 +1,28 @@
-class Offers {
+class Offer {
+    /**
+     * Offer constructor.
+     * 
+     * @param {string} productCode - The code of the product the offer applies to.
+     * @param {string} type - The type of the offer. Accepted values: 'BOGO_HALF_PRICE', 'TWO_FOR_ONE'.
+     * @param {number} value - The value of the offer. Currently only used for 'BOGO_HALF_PRICE' type.
+     */
     constructor(productCode, type, value) {
+      /**
+       * The code of the product the offer applies to.
+       * @type {string}
+       */
       this.productCode = productCode;
+
+      /**
+       * The type of the offer.
+       * @type {string}
+       */
       this.type = type;
+
+      /**
+       * The value of the offer. Currently only used for 'BOGO_HALF_PRICE' type.
+       * @type {number}
+       */
       this.value = value;
     }
   
@@ -11,6 +32,6 @@ class Offers {
     }
   }
   
-  module.exports = Offers;
+  module.exports = Offer;
   
   
