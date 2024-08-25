a21547d689c8572020acf16f6a66ee509e02cf1a
M. Raddaoui <140417247+mr-hookie-repo@users.noreply.github.com>
2024-08-25 10:33:04 -0400

    add comments for clariy

--- src/Basket.js
+++ src/Basket.js
@@ -6,6 +6,10 @@ class Basket {
       this.items = [];
     }
   
+    /**
+     * Add a product to the basket
+     * @param {string} productCode
+     */
     add(productCode) {
       const product = this.catalogService.getProduct(productCode);
       if (product) {
@@ -13,16 +17,28 @@ class Basket {
       }
     }
   
+    /**
+     * Calculate the total of all items in the basket, including any discounts and delivery costs
+     * @returns {number} The total cost of all items in the basket
+     */
     total() {
       const items = this.items;
       const itemsLength = items.length;
       let subtotal = 0;
       let discount = 0;
+  
+      // loop through all items in the basket and calculate the subtotal and discount
       for (let i = 0; i < itemsLength; i++) {
-        subtotal += items[i].price;
-        discount += this.offerService.applyOffers([items[i]])[0].price;
+        const item = items[i];
+        subtotal += item.price;
+        // apply any offers to the item and add the discounted price to the total discount
+        discount += this.offerService.applyOffers([item])[0].price;
       }
+  
+      // calculate the delivery cost based on the subtotal
       const deliveryCost = this.deliveryService.calculateDeliveryCost(subtotal - discount);
+  
+      // return the total cost of all items in the basket, including discounts and delivery costs
       return subtotal - discount + deliveryCost;
     }
   }
